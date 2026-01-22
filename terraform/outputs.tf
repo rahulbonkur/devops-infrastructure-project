e7a4c15d8fc3200ca4a9bcf47e883cd5c107aba4
rahulbonkur <rahulbonkur@gmail.com>
2026-01-22 15:36:44 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
-DOCKER_IP = sh(
-  script: "terraform output -raw docker_host_public_ip",
-  returnStdout: true
-).trim()
+output "docker_host_public_ip" {
+  description = "Public IP of Docker Host EC2"
+  value       = aws_instance.docker_host.public_ip
+}
 
-ANSIBLE_IP = sh(
-  script: "terraform output -raw ansible_slave_public_ip",
-  returnStdout: true
-).trim()
+output "ansible_slave_public_ip" {
+  description = "Public IP of Ansible Slave EC2"
+  value       = aws_instance.ansible_slave.public_ip
+}
